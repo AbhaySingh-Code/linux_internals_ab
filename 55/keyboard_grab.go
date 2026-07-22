78497926996ead1844a73b734545f0613d01aaf9
AbhaySingh-Code <rajputabhay9897@gmail.com>
2026-07-22 01:55:28 -0400

    fixed_ctrl_+c

--- 55/keyboard_grab.go
+++ 55/keyboard_grab.go
@@ -24,6 +24,20 @@ func main() {
 	keybind.Initialize(X)
 	root := X.RootWin()
 
+	//Setting up signal handler before grabbing the keyboard
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-sigChan
+		fmt.Println("\n[!] Cntl + C detected! Releasing keyboard grab...")
+		xproto.UngrabKeyboard(X.Conn(), xproto.TimeCurrentTime)
+		X.Conn().Sync()
+		X.Conn().Close()
+		fmt.Println("[+] Released! Exiting cleanly.")
+		os.Exit(0)
+	}()
+
 	fmt.Println("[+] Requesting global keyboard grab via XGrabKeyboard...")
 
 	reply, err := xproto.GrabKeyboard(
@@ -44,20 +58,21 @@ func main() {
 	}
 
 	fmt.Println("[+] Keyboard grabbed globally! Type anywhere - all keys belong to us.")
-	fmt.Println("[+] Press Cntl + C in this terminal to release the grab.")
+	// fmt.Println("[+] Press Cntl + C in this terminal to release the grab.")
 
-	// Release on cntl + x
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-c
-		fmt.Println("\n[+] Releasing keyboard grab and exiting...")
-		xproto.UngrabKeyboard(X.Conn(), xproto.TimeCurrentTime)
-		X.Conn().Close()
-		os.Exit(0)
-	}()
+	// // Release on cntl + x
+	// c := make(chan os.Signal, 1)
+	// signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	// go func() {
+	// 	<-c
+	// 	fmt.Println("\n[+] Releasing keyboard grab and exiting...")
+	// 	xproto.UngrabKeyboard(X.Conn(), xproto.TimeCurrentTime)
+	// 	X.Conn().Close()
+	// 	os.Exit(0)
+	// }()
 
 	//Attach event handler for keypress events on root window
+	ctrlPressed := false
 	xevent.KeyPressFun(
 		func(X *xgbutil.XUtil, e xevent.KeyPressEvent) {
 			keyStr := keybind.LookupString(X, e.State, e.Detail)
@@ -65,6 +80,25 @@ func main() {
 				keyStr = fmt.Sprintf("KeyCode(%d)", e.Detail)
 			}
 			fmt.Printf("[GRABBED KEY] Code: %-3d | Char: %s\n", e.Detail, keyStr)
+
+			if e.Detail == 37 || e.Detail == 105 {
+				ctrlPressed = true
+			}
+
+			isEsc := e.Detail == 9
+			isQuitKey := e.Detail == 24 // 'q'
+			isCtrlC := ctrlPressed && (e.Detail == 54)
+			if isEsc || isQuitKey || isCtrlC {
+				fmt.Println("\n[!] Exit trigger detected! Releasing keyboard grab...")
+
+				// Explicitly ungrab keyboard
+				xproto.UngrabKeyboard(X.Conn(), xproto.TimeCurrentTime)
+				X.Conn().Sync()
+				X.Conn().Close()
+
+				fmt.Println("[+] Grab released cleanly. Exiting.")
+				xevent.Quit(X)
+			}
 		},
 	).Connect(X, root)
 
